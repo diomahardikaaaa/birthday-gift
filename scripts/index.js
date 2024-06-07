442213b1b78f0b3844df29502c9cab2fdd3da8bb
diomahardikaaaa <128146133+diomahardikaaaa@users.noreply.github.com>
2024-06-07 21:49:08 +0800

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById("merrywrap");
 const canvasC = document.getElementById("c");
 
 const config = {
-  birthdate: "Jan 08, 2024",
+  birthdate: "Jun 08, 2024",
   name: "JENAAA",
 };
 
